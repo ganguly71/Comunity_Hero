22540950d81408b0faddeff7898aaca43cff626d
Adityava Gangopadhyay <adityava49cse@gmail.com>
2026-06-24 18:15:45 +0530

    Increase password_hash column size to 256

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     points = db.Column(db.Integer, default=0)
     role = db.Column(db.String(20), default='citizen') # 'citizen' or 'authority'
     
